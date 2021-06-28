7ee8ebbb060eec297c2ff86669a7ac048abffc60
esdras.jesus <esdrasrubro@outlook.com>
2021-06-28 20:38:52 -0300

    sprint final hehe

--- script.js
+++ script.js
@@ -31,4 +31,19 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
+};
+
+import Countdown from "./countdown.js";
+
+const tempoParaOWWB = new Countdown("18 December 2021 20:00:00 GMT-0300");
+const tempos = document.querySelectorAll("[data-time]");
+
+function mostrarTempo() {
+  tempos.forEach((tempo, index) => {
+    tempo.innerHTML = tempoParaOWWB.total[index];
+  });
 }
+
+mostrarTempo();
+setInterval(mostrarTempo, 1000);
+
